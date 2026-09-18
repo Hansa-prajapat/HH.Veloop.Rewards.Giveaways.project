a6f780c2088ad58ed414352c3d4f99d28a7d6096
Hansa Prajapat <prajapathansa13@gmail.com>
2026-09-18 21:04:30 +0530

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,5 @@
-import React from 'react';import{createRoot}from'react-dom/client';import App from'./App';import'./styles/globals.scss';createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import App from './App';
+import './style.css';
+createRoot(document.getElementById('root')).render(<React.StrictMode><App/></React.StrictMode>);
